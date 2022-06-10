f418924617452aee97accf3c7b927b12ccfb2e23
Isael Jimenez <isaeljimenez94@gmail.com>
2022-06-09 20:25:53 -0500

    taller4 app1

---
+++ taller4a/App01.java
@@ -0,0 +1,24 @@
+package taller4a;
+
+import javax.swing.*;
+
+public class App01 {
+    private JButton pulsameButton;
+    private JPanel panel1;
+
+
+    private  JFrame frame;
+
+    public App01(){
+        frame = new JFrame();
+        frame.add(panel1);
+
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        App01 app = new App01();
+    }
+}
